393b73e76e4a4463bc646dfe9a28c2c8b2c29816
filipnystrom <filipholgernystrom@gmail.com>
2022-04-28 07:05:01 +0200

    changed landing page and added analysis page

--- ui/src/App.js
+++ ui/src/App.js
@@ -5,18 +5,20 @@ import Fragment from "./Components/pages/Fragment";
 import About from "./Components/pages/About";
 import Code from "./Components/pages/Code";
 import CodeBook from "./Components/pages/CodeBook";
+import Analysis from "./Components/pages/Analysis";
 
 function App() {
   return (
     <div className="app-container">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<CodeBook />}/>
+          <Route path="/" element={<About />}/>
           <Route path="/codes" element={<CodeBook />} />
           <Route path="/codes/:id" element={<Code />} />
           <Route path="/about" element={<About />} />
           <Route path="/fragment/:id" element={<Fragment />} />
           <Route path="/post/:id" element={<Thread />} />
+          <Route path="/analysis" element={<Analysis />} />
           <Route path="*" element={() => <>No Page | 404</>} />
         </Routes>
       </BrowserRouter>
